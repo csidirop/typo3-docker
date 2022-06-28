d3f752cfb4623d93f2e7a8b2df4f374f90bfdcc5
Christos <csidirop@runbox.com>
2022-06-28 11:27:32 +0200

    Update docker-entrypoint.sh
    (cherry picked from commit Working docker compose updateb60cd3086249c58da5fc7189a6502bbf3bded1d9)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,20 +4,20 @@
 
 echo '[MAIN] Running startup script:'
 
-# Wait for database container:
+# Get waiting script
+echo '[MAIN] get waiting script:'
 apt-get update
 apt-get install -y wget
 wget -q https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh 
-chmod +x wait-for-it.sh
-./wait-for-it.sh -t 0 ${DB_ADDR}:${DB_PORT}
-
 # Cleanup:
 echo '[MAIN] cleanup:'
 apt-get purge -y wget
 apt-get autoremove -y 
 apt-get clean 
 rm -rf /var/lib/apt/lists/*
-
+# Wait for database container:
+chmod +x wait-for-it.sh
+./wait-for-it.sh -t 0 ${DB_ADDR}:${DB_PORT}
 
 # Database configuration:
 echo '[MAIN] DB configuration:'
